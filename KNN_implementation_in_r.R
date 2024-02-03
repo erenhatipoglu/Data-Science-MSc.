a8cd47af2fe90dc48598e63d209bd057f1cb6601
erenhatipoglu <134336208+erenhatipoglu@users.noreply.github.com>
2024-02-04 00:28:54 +0300

    Create KNN_implementation_in_r.R

---
+++ KNN_implementation_in_r.R
@@ -0,0 +1,44 @@
+# https://www.kaggle.com/datasets/yasserh/wine-quality-dataset
+library(caret)
+library(class)
+library(caTools)
+library(psych)
+
+wine <- read.csv("C:\\Users\\erenh\\OneDrive\\lenova\\deu data science YL\\denetimli öğrenme\\ödev2\\wine.csv")
+
+describe(wine)
+
+# Data Preprocessing
+
+# Removal of the ID column
+wine <- subset(wine, select = -c(Id))
+# Quality column is converted to factor from integer.
+wine$quality <- as.factor(wine$quality)
+
+set.seed(255)
+
+# Train-Test Split
+split <- sample.split(wine$quality, SplitRatio = 0.7)
+train <- subset(wine, split == TRUE)
+test <- subset(wine, split == FALSE)
+
+# # Feature Scaling
+# (x - mean(x)) / sd(x) which is Z-Score
+train_scaled <- scale(train[-12])
+test_scaled <- scale(test[-12])
+
+# Square Root of N to find out the best K value
+n <- nrow(train)
+best_k <- round(sqrt(n))
+print(best_k)
+
+# KNN Model
+test_pred <- knn(train = train_scaled, 
+                 test = test_scaled,
+                 cl = train$quality, 
+                 k = best_k)
+
+# Metrics
+actual <- test$quality
+cm <- confusionMatrix(as.factor(test_pred), actual)
+print(cm)
